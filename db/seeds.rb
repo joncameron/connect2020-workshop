9de0b6170af5be7123fbd38fa2a1f61983ac38b5
Phuong Dinh <phuongdh@gmail.com>
2020-10-20 13:03:10 -0400

    Change streams location

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Item.create({ title: "My video test", 
-              streams: { "240p" => "http://localhost:3333/samvera/240p.mp4/index.m3u8", 
-                         "480p" => "http://localhost:3333/samvera/480p.mp4/index.m3u8",
-                         "1080p" => "http://localhost:3333/samvera/1080p.mp4/index.m3u8"}})
+              streams: { "240p" => "http://localhost:3333/samvera/bunny/240p.mp4/index.m3u8", 
+                         "480p" => "http://localhost:3333/samvera/bunny/480p.mp4/index.m3u8",
+                         "1080p" => "http://localhost:3333/samvera/bunny/1080p.mp4/index.m3u8"}})
 
 User.create(email: "user@samvera.org", password: "connect2020")
